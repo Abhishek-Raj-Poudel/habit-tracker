69594089de9dc066ffc0c0f8f19210d791f512dd
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2021-12-15 20:00:15 +0545

    FINALLY The Css is fixed (Some what! 😂😂)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./app.css";
 import Sidebar from "./Sidebar";
 import Task from "./Task";
+import TaskComplete from "./TaskComplete";
 import Details from "./Details";
 
 function App() {
   return (
     <div className="app">
-      <Sidebar className="sidebar" />
-      <Task className="task" />
+      <Sidebar className="app__sidebar" />
+      <div className="app__tasks">
+        <Task />
+        <TaskComplete />
+      </div>
       <Details className="details" />
     </div>
   );
---
+++ src/TaskComplete.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./task.css";
+
+function TaskComplete() {
+  return <div className="task">Tasks Complete</div>;
+}
+
+export default TaskComplete;
